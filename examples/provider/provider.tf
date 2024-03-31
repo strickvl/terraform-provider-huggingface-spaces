71807c989fd35b7d800430b7bb6594a34751b1dd
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-03-31 09:42:40 +0200

    add examples

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,17 @@
-provider "scaffolding" {
-  # example configuration here
+terraform {
+  required_providers {
+    huggingface-spaces = {
+      source = "strickvl/huggingface-spaces"
+    }
+  }
+}
+
+provider "huggingface-spaces" {
+  token = var.huggingface_token
+}
+
+variable "huggingface_token" {
+  type        = string
+  description = "The Hugging Face API token."
+  sensitive   = true
 }
